2b326517a3330aa5e5d60701c151015b3fb73257
ghostm <henderson.matthew@gmail.com>
2010-09-20 18:16:40 -0600

    Update to Lift 2.1RC2

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,21 +1,22 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val mavenLocal = "Local Maven Repository" at
-  "file://"+Path.userHome+"/.m2/repository"
+  val liftVersion = "2.1-RC2"
 
-  val scalatools_release = "Scala Tools Snapshot" at
-  "http://scala-tools.org/repo-releases/"
+  // uncomment the following if you want to use the snapshot repo
+  //val scalatoolsSnapshot = ScalaToolsSnapshots
 
-  val liftVersion = "2.0"
+  // If you're using JRebel for Lift development, uncomment
+  // this line
+  // override def scanDirectories = Nil
 
   override def libraryDependencies = Set(
-    "net.liftweb" % "lift-webkit" % liftVersion % "compile->default",
-    "net.liftweb" % "lift-couchdb" % liftVersion % "compile->default",
-    "net.liftweb" % "lift-mapper" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default",
+	"net.liftweb" %% "lift-couchdb" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
-    "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test->default",
-    "com.h2database" % "h2" % "1.2.121"
+    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default",
+    "com.h2database" % "h2" % "1.2.138"
   ) ++ super.libraryDependencies
 }
--- src/main/scala/code/model/ToDo.scala
+++ src/main/scala/code/model/ToDo.scala
@@ -6,26 +6,25 @@ import net.liftweb.record.field._
 import net.liftweb.common._
 import net.liftweb.json.JsonAST.{JField, JInt, JObject, JString, render}
                   
-class ToDo extends CouchRecord[ToDo]{
+class ToDo private () extends CouchRecord[ToDo]{
  def meta = ToDo
  
  object done extends BooleanField(this)
  object owner extends StringField(this, 64)
  object priority extends IntField(this) {
    override def defaultValue = 5
-   override def validators = validPriority _ :: super.validators
+   override def validations = validPriority _ :: super.validations
 
-   def validPriority(in: Box[Int]): List[FieldError] =
-    if (in.open_! > 0 && in.open_! <= 10) Nil
+   def validPriority(in: Int): List[FieldError] =
+    if (in > 0 && in <= 10) Nil
     else List(FieldError(this, <b>Priority must be 1-10</b>))
  } 
  object desc extends StringField(this, 128) {
-   override def validators =
+   override def validations =
     validLength _ ::
-      super.validators
-   def validLength(in: Box[String]): List[FieldError] = {
-    val inString = in.open_!
-    if (inString.size >= 3) Nil
+      super.validations
+   def validLength(in: String): List[FieldError] = {
+    if (in.size >= 3) Nil
     else List(FieldError(this, <b>Description must be at least 3 characters</b>))
    }
 
@@ -33,7 +32,6 @@ class ToDo extends CouchRecord[ToDo]{
 }
  
 object ToDo extends ToDo with CouchMetaRecord[ToDo]{
-  def createRecord = new ToDo
   lazy val priorityList = (1 to 10).map(v => (v.toString, v.toString))
 
   def findAll(tempOwnerBox:Box[User], excludeDone:Boolean):List[ToDo] = {
