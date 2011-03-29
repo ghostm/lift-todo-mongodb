b193886f6f2b4aeb77b5fe9531883ccfd11d7c30
ghostm <henderson.matthew@gmail.com>
2011-03-28 19:06:40 -0600

    Project settings update. Closes gh-1.

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.2"
+  val liftVersion = "2.3-RC3"
 
   // uncomment the following if you want to use the snapshot repo
   //val scalatoolsSnapshot = ScalaToolsSnapshots
@@ -16,6 +16,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
 	"net.liftweb" %% "lift-mongodb-record" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
-    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default",
+    "org.scala-tools.testing" %% "specs" % "1.6.7" % "test->default",
+	"ch.qos.logback" % "logback-classic" % "0.9.26"
   ) ++ super.libraryDependencies
 }
